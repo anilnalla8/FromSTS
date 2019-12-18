ecf286397f53088321d4d4b38706a242c2b5e834
Bobby <anilnalla8@gmail.com>
2019-12-18 20:00:16 +0530

    First commit

---
+++ SpringBootApp/src/main/java/com/example/demo/WelcomeRestController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/welcome")
+public class WelcomeRestController {
+
+	public WelcomeRestController() {
+		System.out.println("WelcomeRestController : : Constructor");
+	}
+	
+	@GetMapping(value= "{name}")
+	public String welcome(@PathVariable String name) {
+		String msg= "Hi " +name+ ", Welcome to REST API.....";
+		return msg;
+	}
+}
